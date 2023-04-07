2f85a1e9b5de4666cedb80c6817b7512ce819fd6
CubesAndCubes <120466698+CubesAndCubes@users.noreply.github.com>
2023-04-07 19:27:13 +0200

    feat(LILA): Add #tokenize() function

--- LILA.js
+++ LILA.js
@@ -7,7 +7,28 @@ export class LILA {
         return this.memory.length += chunks - chunks;
     }
 
-    eval(script) {
+    #tokenize(script) {
+        const result = [];
 
+        let temp = '';
+
+        for (const current of script) {
+            temp += current;
+
+            let tokenFound = false;
+
+            switch(temp) {
+                
+            }
+
+            if (tokenFound)
+                temp = '';
+        }
+
+        return result;
+    }
+
+    eval(script) {
+        const tokens = this.#tokenize(script);
     }
 }
