6fafea80c7497e6581cccfa1bb36625caf88c415
CubesAndCubes <120466698+CubesAndCubes@users.noreply.github.com>
2023-04-08 21:44:44 +0200

    feat(LILA): Add subtract() function

--- LILA.js
+++ LILA.js
@@ -43,6 +43,13 @@ export class LILA {
         );
     }
 
+    subtract(destination, value) {
+        this.move(
+            destination,
+            this.retrieve(destination) - LILA.#normalizeValue(value),
+        );
+    }
+
     stackMemory = [];
 
     push(value) {
