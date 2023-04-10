7254517393b739ffaf66d47fc2ce18b65ddd1b15
Cubiii <120466698+CubesAndCubes@users.noreply.github.com>
2023-04-10 21:44:23 +0200

    feat(TokenTypes): Add colon type

--- LILA.js
+++ LILA.js
@@ -125,6 +125,7 @@ export class LILA {
     static #TokenTypes = {
         whitespace: /^\s+/,
         comment: /^;.*/,
+        colon: /^:/,
         identifier: /^[_A-Za-z][_A-Za-z\d]*/,
     }
 
