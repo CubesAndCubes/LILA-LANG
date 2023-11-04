4e7dc031d3a1ac855d88785a73059ba5879d57ac
Cubiii <120466698+CubesAndCubes@users.noreply.github.com>
2023-11-04 14:31:07 +0100

    feat(token_types): Add more

--- src/token_types.js
+++ src/token_types.js
@@ -4,16 +4,24 @@ export const whitespace = lexer.register_token_type(
     /^[^\S\n]+/,
 );
 
+export const comment = lexer.register_token_type(
+    /^;.*/,
+);
+
+export const identifier = lexer.register_token_type(
+    /^[_A-Za-z][_A-Za-z\d]*/,
+);
+
+export const number = lexer.register_token_type(
+    /^(-[^\S\n]*)?\d+(\.\d+)?/,
+);
+
+export const string = lexer.register_token_type(
+    /^"[^"]+"|'[^']+'/,
+);
+
 export const line_break = lexer.register_token_type(
     /^\n/,
-);
-
-export const comma = lexer.register_token_type(
-    /^\,/,
-);
-
-export const colon = lexer.register_token_type(
-    /^\:/,
 );
 
 export const opening_square_bracket = lexer.register_token_type(
@@ -24,14 +32,26 @@ export const closing_square_bracket = lexer.register_token_type(
     /^\]/,
 );
 
+export const operator = lexer.register_token_type(
+    /^\+|\-|\*|\/|\%/,
+);
+
+export const comma = lexer.register_token_type(
+    /^\,/,
+);
+
+export const colon = lexer.register_token_type(
+    /^\:/,
+);
+
+export const dollar = lexer.register_token_type(
+    /^\$/,
+);
+
 export const opening_parenthesis = lexer.register_token_type(
     /^\(/,
 );
 
 export const closing_parenthesis = lexer.register_token_type(
-    /^\)/,
-);
-
-export const identifier = lexer.register_token_type(
     /^\)/,
 );
